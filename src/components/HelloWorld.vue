76d517b940c256ac8df294008b2c07827819c34e
zpzhang <chee7@qq.com>
2022-04-13 11:19:14 +0800

    [master]1、logic-flow demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref,onMounted } from 'vue'
+import LogicFlow from "@logicflow/core";
+
+defineProps<{ msg: string }>()
+
+onMounted(() => {
+  const container : HTMLElement = document.querySelector('#lf') as HTMLElement
+  const lf = new LogicFlow({
+    container
+  });
+  const graphData = {
+    nodes: [
+      {
+        id: "node_id_1",
+        type: "rect",
+        x: 100,
+        y: 100,
+        text: { x: 100, y: 100, value: '节点1' },
+        properties: {}
+      },
+      {
+        id: "node_id_2",
+        type: "circle",
+        x: 200,
+        y: 300,
+        text: { x: 300, y: 300, value: '节点2' },
+        properties: {}
+      }
+    ],
+    edges: [
+      {
+        id: "edge_id",
+        type: "polyline",
+        sourceNodeId: "node_id_1",
+        targetNodeId: "node_id_2",
+        text: { x: 139, y: 200, value: "连线" },
+        startPoint: { x: 100, y: 140 },
+        endPoint: { x: 200, y: 250 },
+        pointsList: [ { x: 100, y: 140 }, { x: 100, y: 200 }, { x: 200, y: 200 }, { x: 200, y: 250 } ],
+        properties: {}
+      }
+    ]
+  }
+  lf.render(graphData)
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div id="lf" style="{
+    width: 1000px;
+    height: 500px
+  }"></div>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
